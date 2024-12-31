b1335ab0310f70be6bfca3bca82dc81c5db4abcf
Michal Kochnio <kochniomichal@gmail.com>
2024-12-31 09:58:31 +0100

    add 'toFixed' function

--- BMI calculator/code.js
+++ BMI calculator/code.js
@@ -4,10 +4,12 @@ let pounds = window.prompt("Your weight in pounds?");
 cm = inches * 2.54;
 kg = pounds / 2.2046;
 
-console.log("Your height in cm " + cm + ", your weight in kg " + kg);
+console.log(
+  "Your height in cm " + cm.toFixed() + ", your weight in kg " + kg.toFixed()
+);
 
 bmi = kg / (cm / 100) ** 2;
-console.log("Your BMI is " + bmi);
+console.log("Your BMI is " + bmi.toFixed(2));
 
 //174cm - 68.5
 //75 - 165.35
